abda4c2a5285edbc96ce31773d6e7a01bdc986d8
VenkataSubhash96 <subhashbharadwaj96@gmail.com>
2025-08-01 10:42:55 +0200

    Add spec for returning the sum of two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe StringCalculator do
   it "returns the number itself for a single number" do
     expect(calculator.add("5")).to eq(5)
   end
+
+  it "returns the sum of two numbers" do
+    expect(calculator.add("1,2")).to eq(3)
+  end
 end
